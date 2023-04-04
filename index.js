4e9e2ef160d514f7bf55146101a1fc1624e9a1e3
Pavel Chernov <pvchernoff@gmail.com>
2023-04-04 09:39:26 +0300

    feat: add log

--- index.js
+++ index.js
@@ -7,47 +7,67 @@ dotenv.config();
 const __dirname = path.resolve();
 const token = process.env.TELEGRAM_TOKEN;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 const sentGifs = {};
 
 const menu = {
   reply_markup: {
-    keyboard: [
-      [
-        { text: 'Получить гиф' },
-      ],
-    ],
+    keyboard: [[{ text: 'Получить гиф' }]],
     resize_keyboard: true,
     one_time_keyboard: false,
   },
+};
+
+// Создаем папку logs, если она не существует
+const logsDir = path.join(__dirname, 'logs');
+if (!fs.existsSync(logsDir)) {
+  fs.mkdirSync(logsDir);
 }
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Привет! Я бот, который ищет в памяти вашего смартфона и отправляет случайный gif'. Нажми кнопку 'Получить гиф', чтобы начать.", menu);
+  bot.sendMessage(
+    chatId,
+    "Привет! Я бот, который ищет в памяти вашего смартфона и отправляет случайный gif'. Нажми кнопку 'Получить гиф', чтобы начать.",
+    menu
+  );
 });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  if (msg.text === 'Получить гиф', menu) {
+  if ((msg.text === 'Получить гиф', menu)) {
     if (!sentGifs[chatId]) {
       sentGifs[chatId] = [];
     }
 
     const gifs = fs.readdirSync(path.join(__dirname, 'gifs'));
 
-    const filteredGifs = gifs.filter((gif, index) => !sentGifs[chatId].includes(index));
+    const filteredGifs = gifs.filter(
+      (gif, index) => !sentGifs[chatId].includes(index)
+    );
 
     if (filteredGifs.length === 0) {
       sentGifs[chatId] = [];
-      return bot.sendMessage(chatId, 'Все gif уже были отправлены, начинаю сначала', menu);
+      return bot.sendMessage(
+        chatId,
+        'Все gif уже были отправлены, начинаю сначала',
+        menu
+      );
     }
 
-    const randomGif = filteredGifs[Math.floor(Math.random() * filteredGifs.length)];
+    const randomGif =
+      filteredGifs[Math.floor(Math.random() * filteredGifs.length)];
 
     bot.sendDocument(chatId, path.join(__dirname, 'gifs', randomGif));
 
     sentGifs[chatId].push(gifs.indexOf(randomGif));
+
+    // Записываем активность пользователя в файл activity.log
+    const username = msg.from.username || 'Unknown user';
+    const logMsg = `User ${username} requested a gif at ${new Date().toISOString()}\n`;
+    fs.appendFile(path.join(logsDir, 'activity.log'), logMsg, (err) => {
+      if (err) console.error(err);
+    });
   }
 });
